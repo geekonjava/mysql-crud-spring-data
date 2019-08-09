b0d71e41989fe3323222498f69b44edfa69b0a50
geekonjava <sawankarn123@gmail.com>
2019-08-09 14:19:24 +0530

    Update MysqlCrudSpringDataApplication.java

--- mysql-crud-spring-data/src/main/java/com/mysql/demo/mysqlcrudspringdata/MysqlCrudSpringDataApplication.java
+++ mysql-crud-spring-data/src/main/java/com/mysql/demo/mysqlcrudspringdata/MysqlCrudSpringDataApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class MysqlCrudSpringDataApplication {
-
+	//Comment from github
 	public static void main(String[] args) {
 		SpringApplication.run(MysqlCrudSpringDataApplication.class, args);
 	}
